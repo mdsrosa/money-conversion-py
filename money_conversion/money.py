a74e91613be376d6d71fb90c15cab689af661e37
Matheus Rosa <matheusdsrosa@gmail.com>
2015-12-15 20:20:59 -0200

    Add __getattr__ method in order to be able to call non-defined methods

--- money_conversion/money.py
+++ money_conversion/money.py
@@ -10,11 +10,16 @@ class Money(object):
     def __repr__(self):
         return "%.2f %s" % (self.amount, self.currency)
 
-    def to_currency(self, new_currency):
-        new_currency = new_currency.split('_')[1].upper()
+    def __getattr__(self, currency):
+        def convert():
+            return self.to_currency(currency)
+        return convert
+
+    def to_currency(self, currency):
+        currency = currency.split('_')[1].upper()
         amount = self.amount
         base_currency_rates = rates.get(self.currency)
 
-        new_amount = amount * base_currency_rates.get(new_currency)
+        new_amount = amount * base_currency_rates.get(currency)
 
-        return Money(new_amount, new_currency)
+        return Money(new_amount, currency)
