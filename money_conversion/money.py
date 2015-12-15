3befcbaf3a78a46edc31cc1910fcd8e0a9381102
Matheus Rosa <matheusdsrosa@gmail.com>
2015-12-15 20:18:53 -0200

    Add to_currency method in order to be able to convert to a new currency

--- money_conversion/money.py
+++ money_conversion/money.py
@@ -1,3 +1,5 @@
+from currency_rates import rates
+
 
 class Money(object):
 
@@ -7,3 +9,12 @@ class Money(object):
 
     def __repr__(self):
         return "%.2f %s" % (self.amount, self.currency)
+
+    def to_currency(self, new_currency):
+        new_currency = new_currency.split('_')[1].upper()
+        amount = self.amount
+        base_currency_rates = rates.get(self.currency)
+
+        new_amount = amount * base_currency_rates.get(new_currency)
+
+        return Money(new_amount, new_currency)
