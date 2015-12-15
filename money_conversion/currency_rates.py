29234407d5cc9b4d9425996aabfc55a3c314cf4d
Matheus Rosa <matheusdsrosa@gmail.com>
2015-12-15 20:05:29 -0200

    Add currency rates file

---
+++ money_conversion/currency_rates.py
@@ -0,0 +1,19 @@
+# Currency rates on 2015-12-15
+
+rates = {
+    'EUR': {
+        'USD': 1.09,
+        'BRL': 4.25
+    },
+
+    'USD': {
+        'EUR': 0.91,
+        'BRL': 3.9
+    },
+
+    'BRL': {
+        'EUR': 0.25,
+        'USD': 0.25
+    }
+}
+
