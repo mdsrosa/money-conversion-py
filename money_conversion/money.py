836b7a118f38e8dd140e81da6df345a0c556f409
Matheus Rosa <matheusdsrosa@gmail.com>
2015-12-22 15:19:58 -0200

    Add sum operation between Money objects

--- money_conversion/money.py
+++ money_conversion/money.py
@@ -17,14 +17,22 @@ class Money(object):
         currency = currency.split('_')[1].upper()
 
         def convert():
-            return self.to_currency(currency)
+            return self._to_currency(currency)
 
         return convert
 
-    def to_currency(self, currency):
+    def _to_currency(self, currency):
         amount = self.amount
         base_currency_rates = rates.get(self.currency)
 
         new_amount = amount * base_currency_rates.get(currency)
 
         return Money(new_amount, currency)
+
+    def __add__(self, money_obj):
+        assert isinstance(money_obj, Money)
+
+        new_money = money_obj._to_currency(self.currency)
+        amount = self.amount + new_money.amount
+
+        return Money(amount, self.currency)
--- tests.py
+++ tests.py
@@ -20,11 +20,33 @@ class MoneyClassTest(unittest.TestCase):
         self.assertEqual('BRL', twenty_brl.currency)
         self.assertEqual(85, twenty_brl.amount)
 
-
     def test_invalid_method_pattern_call(self):
         with self.assertRaises(AttributeError):
             twenty_brl = self.twenty_euro.batman()
 
+    def test_sum_operation_between_eur_and_dollar(self):
+        fifty_eur = Money(50, 'EUR')
+        twenty_dollars = Money(20, 'USD')
+
+        money = fifty_eur + twenty_dollars
+
+        self.assertEqual(money.amount, 68.2)
+        self.assertEqual(money.currency, 'EUR')
+
+        # test if a new object is returned
+        self.assertNotEqual(id(fifty_eur), id(money))
+
+    def test_sum_operation_between_dollar_and_eur(self):
+        fifty_eur = Money(50, 'EUR')
+        twenty_dollars = Money(20, 'USD')
+
+        money = twenty_dollars + fifty_eur
+
+        self.assertEqual(money.amount, 74.5)
+        self.assertEqual(money.currency, 'USD')
+
+        # test if a new object is returned
+        self.assertNotEqual(id(fifty_eur), id(money))
 
 if __name__ == '__main__':
     unittest.main()
